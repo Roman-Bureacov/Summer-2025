d1d9531ea51747baa02749a2e26f1ccab18ed075
RomaBureacov <RomanBureacov@gmail.com>
2025-07-02 20:39:58 -0700

    basic test class

---
+++ TCSS 360/assignment1Minesweeper/test/minesweeper/HintGeneratorTest.java
@@ -0,0 +1,72 @@
+/*
+    Summer 2025
+    testing class for the static class HintGenerator
+ */
+
+package minesweeper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Testing class for the minesweeper hint generator.
+ *
+ * @author Roman Bureacov
+ * @version July 2025
+ */
+public final class HintGeneratorTest {
+    String mySampleField;
+    String myExpectedField;
+
+    @Test
+    public void testMinefield1() {
+        mySampleField =
+                """
+                4 5
+                .....
+                .....
+                *....
+                ...*.
+                0 0
+                """;
+        myExpectedField =
+                """
+                Field #1:
+                00000
+                11000
+                *1111
+                111*1
+                
+                """;
+
+        test();
+    }
+
+    private void test() {
+        // redirect input and output
+        final ByteArrayInputStream input = new ByteArrayInputStream(mySampleField.getBytes());
+        System.setIn(input);
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        final PrintStream printStream = new PrintStream(output);
+        System.setOut(printStream);
+
+        // get output
+        HintGenerator.main();
+
+        // normalize newlines
+        final String expectedOut = output.toString().replaceAll("\r", "");
+        myExpectedField = myExpectedField.replaceAll("\r", "");
+
+        assertEquals(
+                myExpectedField,
+                expectedOut,
+                "Unequal minefields"
+        );
+    }
+}
